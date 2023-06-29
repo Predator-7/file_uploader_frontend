211540aa2f0ef6b1bea2631543aa68fc27297ff3
Predator-7 <anshgarewal7@gmail.com>
2023-06-29 22:32:14 +0530

    api links updated

--- src/assets/api.js
+++ src/assets/api.js
@@ -1,11 +1,11 @@
 const API_ENDPOINTS = {
-    getUser: '/api/user',
-    signup: "http://localhost:8080/api/signup",
-    login: "http://localhost:8080/api/login",
-    getAllPdf : "http://localhost:8080/api/uploadedFiles",
-    upload: "http://localhost:8080/api/upload",
-    deletePdf: "http://localhost:8080/api/file",
-    download: "http://localhost:8080/api/files/"
+    // getUser: 'https://file-uploader-q4n7.onrender.com/api/user',
+    signup: "https://file-uploader-q4n7.onrender.com/api/signup",
+    login: "https://file-uploader-q4n7.onrender.com/api/login",
+    getAllPdf : "https://file-uploader-q4n7.onrender.com/api/uploadedFiles",
+    upload: "https://file-uploader-q4n7.onrender.com/api/upload",
+    deletePdf: "https://file-uploader-q4n7.onrender.com/api/file",
+    download: "https://file-uploader-q4n7.onrender.com/api/files/"
   };
   
   export default API_ENDPOINTS;
